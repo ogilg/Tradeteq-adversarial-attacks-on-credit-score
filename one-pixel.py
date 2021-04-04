a66db306d2985f61d59c469e9402db5d09c70227
Yusi-N <47685414+Yusi-N@users.noreply.github.com>
2021-04-04 17:26:08 +0800

    Add files via upload

---
+++ one-pixel.py
@@ -0,0 +1,58 @@
+
+import pandas as pd
+import numpy as np
+import os
+import math
+from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+class simBA:
+    def _init_(self, targetModel):
+
+        self.targetModel = targetModel
+        trainingData = pickle.load(open(os.path.join("Processing", "train_preproc.p"), "rb"))
+		testData = pickle.load(open(os.path.join("Processing", "test_preproc.p"), "rb"))
+		self.columns = trainingData[0].columns
+		X_trainraw, y_trainraw = trainingData[0].to_numpy(), trainingData[1]
+		X_testraw, y_testraw = testData[0].to_numpy(), testData[1]
+		X = np.vstack((X_trainraw, X_testraw))
+		y = np.hstack((y_trainraw, y_testraw))
+		_, X_sample, _, y_sample = train_test_split(X, y, test_size=0.01, random_state=0, stratify=y)
+		self.perturb(X_sample, y_sample)
+        
+    
+    def test_accuracy(self, X, y):
+        print(f"Accuracy - Test: {accuracy_score(y, self.targetModel.predict(X))}")
+        test_auc = roc_auc_score(y, self.targetModel.predict_proba(X)[:, 1])
+        print(f"AUC - Test: {test_auc}")
+
+    def train(self, X, y):
+        col=random(randint(0,len(X)-1))
+        feature=X[col]
+        if type(feature)==str :
+            feature=feature[::-1]
+
+        else :
+            feature=feature*(-1)
+        X[col]=feature
+        
+        
+if _name_== '__main__':
+    
+    # dummy target model to test against
+    trainingData = pickle.load(open(os.path.join("Processing", "train_preproc.p"), "rb"))
+    testData = pickle.load(open(os.path.join("Processing", "test_preproc.p"), "rb"))
+    X_trainraw, y_trainraw = trainingData[0].to_numpy(), trainingData[1]
+    X_testraw, y_testraw = testData[0].to_numpy(), testData[1]
+    X = np.vstack((X_trainraw, X_testraw))
+    y = np.hstack((y_trainraw, y_testraw))
+    
+    # lr = LogisticRegression(max_iter=10000)
+    # lr.fit(X, y)
+    xg = pickle.load(open(os.path.join("fitted_models", "xg_0.927.pkl"), "rb"))
+    
+    
+    
+    
