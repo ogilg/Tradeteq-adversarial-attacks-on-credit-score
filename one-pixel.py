8bd5a6b272df5245c9c15aeb09a8e1bf1112b6dc
Yusi-N <47685414+Yusi-N@users.noreply.github.com>
2021-04-25 22:59:23 +0800

    Update one-pixel.py

--- one-pixel.py
+++ one-pixel.py
@@ -3,17 +3,18 @@ import pandas as pd
 import numpy as np
 import os
 import math
+import sklearn
+import xgboost
+import pickle
 from sklearn.metrics import accuracy_score, roc_auc_score
 from sklearn.model_selection import train_test_split
 from numpy.random import choice
-import pickle
-import random
+
 
 class onePixel:
-    def __init__(self, weight, targetModel):
+    def __init__(self, targetModel):
 
         self.targetModel = targetModel
-        self.weight=weight
 
         trainingData = pickle.load(open(os.path.join("Processing", "train_preproc.p"), "rb"))
         testData = pickle.load(open(os.path.join("Processing", "test_preproc.p"), "rb"))
@@ -26,8 +27,8 @@ class onePixel:
 
 
 
-    def perturb(self, X, weight):
-        col = choice(range(0,len(X)), 1, weight)
+    def perturb(self, X):
+        col = choice(len(X), 1)
         feature = X[col]
         if type(feature) == str:
             feature = feature[::-1]
@@ -37,13 +38,13 @@ class onePixel:
         return self.targetModel.predict(X)
 
 
-    def test_accuracy(self, X, y, weight):
-        print(f"Accuracy - Test: {accuracy_score(y, self.perturb(X, weight))}")
+    def test_accuracy(self, X, y):
+        print(f"Accuracy - Test: {accuracy_score(y, self.perturb(X))}")
         test_auc = roc_auc_score(y, self.targetModel.predict_proba(X)[:, 1])
         print(f"AUC - Test: {test_auc}")
 
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
     
        # dummy target model to test against
         trainingData = pickle.load(open(os.path.join("Processing", "train_preproc.p"), "rb"))
@@ -54,11 +55,10 @@ class onePixel:
         y = np.hstack((y_trainraw, y_testraw))
     
 
-        xg = pickle.load(open(os.path.join("fitted_models", "xg_0.904"), "rb"))
+
         rf = pickle.load(open(os.path.join("fitted_models","rf_0.929"),"rb"))
-        weight = pickle.load(open("importance.txt","rb"))
-    
-        onePixel = onePixel(xg,weight)
+        onePixel = onePixel(rf)
         onePixel.test_accuracy(X,y)
     
-    
+    #  Accuracy - Test: 0.9980133333333333
+    # AUC - Test: 0.9376627596348743
